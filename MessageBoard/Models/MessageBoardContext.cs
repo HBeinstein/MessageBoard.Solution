6ce1dfb29121b16df6d84daad4e3f43d2459d514
Jeff Kim <kim2jy@hotmail.com>
2020-08-17 11:09:56 -0700

    Add controllers and seed data
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- MessageBoard/Models/MessageBoardContext.cs
+++ MessageBoard/Models/MessageBoardContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace MessageBoard.Models
@@ -12,5 +13,16 @@ namespace MessageBoard.Models
       public DbSet<Message> Messages { get; set; }
       public DbSet<Group> Groups { get; set; }
       public DbSet<GroupMessage> GroupMessages { get; set; }
+
+      protected override void OnModelCreating(ModelBuilder builder)
+      {
+        builder.Entity<Message>()
+        .HasData(
+          new Message { MessageId = 1, Title = "Hello", Body = "Hello? Hello???", Date = DateTime.Today, UserName = "Test1"}
+          new Message { MessageId = 2, Title = "Goodbye", Body = "I don't know why you say goodbye I say hello.", Date = DateTime.Today, UserName = "Test2"}
+          new Message { MessageId = 1, Title = "This is only a test", Body = "This is a test of the emergency alert system.", Date = DateTime.Today, UserName = "Test3"}
+          new Message { MessageId = 1, Title = "If this was a real alert", Body = "You would be dead. Probably, who knows. If you saw a flash most likely.", Date = DateTime.Today, UserName = "Test4"}
+        )
+      }
   }
 }
