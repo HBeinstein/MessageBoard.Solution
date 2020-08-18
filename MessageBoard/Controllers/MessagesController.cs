04b5038c19edaf6bd6824804a75f7c5a7ca59bc0
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-18 11:23:17 -0700

    add put and delete methods to messagescontroller.cs
    Co-authored-by: jkbutserious <kim2jy@hotmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- MessageBoard/Controllers/MessagesController.cs
+++ MessageBoard/Controllers/MessagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MessageBoard.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MessageBoard.Controllers
 {
@@ -45,6 +46,24 @@ namespace MessageBoard.Controllers
     public void Post([FromBody] Message message)
     {
       _db.Messages.Add(message);
+      _db.SaveChanges();
+    }
+
+    // PUT api/messages/[MessageId]
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Message message)
+    {
+      message.MessageId = id;
+      _db.Entry(message).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    
+    //DELETE api/messages/[MessageId]
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var messageToDelete = _db.Messages.FirstOrDefault(entry => entry.MessageId == id);
+      _db.Messages.Remove(messageToDelete);
       _db.SaveChanges();
     }
   }
