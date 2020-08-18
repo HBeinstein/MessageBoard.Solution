9e14c617361041c6ab4a3e693b218671dfba0f19
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-18 14:10:37 -0700

    edit values controller
    Co-authored-by: jkbutserious <kim2jy@hotmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- MessageBoard/Controllers/ValuesController.cs
+++ MessageBoard/Controllers/ValuesController.cs
@@ -52,6 +52,7 @@ namespace MessageBoard.Solution.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            
             return new string[] { "value1", "value2" };
         }
     }
